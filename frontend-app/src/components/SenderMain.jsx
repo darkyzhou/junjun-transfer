5d53ede6727fd6b1a1e3205dd7e745cf31ae317d
darkyzhou <darkyoooooo@gmail.com>
2021-05-17 20:26:45 +0800

    feat: prevent empty file transfer

--- frontend-app/src/components/SenderMain.jsx
+++ frontend-app/src/components/SenderMain.jsx
@@ -56,7 +56,16 @@ export const SenderMain = ({ socket, jobId }) => {
     <main className="p-4 md:p-8 lg:p-12 grid grid-cols-2 grid-rows-1 gap-4">
       <div className="flex flex-col items-center">
         <h4 className="text-gray-200 font-zcool text-3xl tracking-widest mb-4 sm:mb-8">发送文件</h4>
-        {!selectedFile && <SenderInstructionPanel className="flex-1" onSelectFile={(file) => setSelectedFile(file)} />}
+        {!selectedFile && (
+          <SenderInstructionPanel
+            className="flex-1"
+            onSelectFile={(file) => {
+              if (file.size > 0) {
+                setSelectedFile(file);
+              }
+            }}
+          />
+        )}
         {selectedFile && (
           <SenderSelectedFileCard
             className="flex-1"

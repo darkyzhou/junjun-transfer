1d709e5cbfd16ddf6ca9c29f8b128e865c78a36b
darkyzhou <darkyoooooo@gmail.com>
2021-05-20 17:43:55 +0800

    feat: rename signal-server & add dockerfile

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  build: { target: 'es2018' },
   server: {
     proxy: {
       '/signal': {

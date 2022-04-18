2f996ddaa7698f482214a1cbe60d2c48aa1d0be6
darkyzhou <darkyoooooo@gmail.com>
2022-04-18 22:30:46 +0800

    chore: use server to serve static files

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,17 @@
-FROM node:alpine as fe_build
-COPY frontend-app/package.json /fe_build/
-WORKDIR /fe_build
+FROM node:alpine as build
+WORKDIR /app
+COPY frontend/package.json /app
 RUN npm install
-COPY frontend-app /fe_build
+COPY frontend /app
 RUN npm run build
 
-FROM nginx:latest
-RUN apt-get update && \
-    apt-get install -y curl sudo
-RUN curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && \
-    sudo apt-get install -y nodejs && \
-    echo "Installed node: " && \
-    node --version && \
-    npm install -g pnpm
+FROM node:alpine
+WORKDIR /app
+COPY server /app
+RUN npm install
+COPY --from=build /app/dist /app/public
 
-COPY default.conf /etc/nginx/conf.d
-COPY --from=fe_build /fe_build/dist /www/frontend
-
-COPY backend-app /backend-app
-WORKDIR /backend-app
-RUN pnpm install
-
-EXPOSE 80
-CMD nginx && \
-    echo "Nginx started, now starting backend-app..." && \
-    node index.js
+EXPOSE 8080
+CMD ["node", "index.js"]
 
 LABEL org.opencontainers.image.authors="darkyoooooo@gmail.com"
-
--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
-const startupSignalServer = require('./lib/signal-server');
-const startupIceAdvertiser = require('./lib/ice-advertiser');
-const { addExitHook } = require('exit-hook-plus');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
+const helmet = require('helmet');
+const { JobController } = require('./lib/job-controller');
+const servers = require('./config/ice-servers.json'); // TODO: should check validity
 
-startupSignalServer();
-startupIceAdvertiser();
-addExitHook(() => console.log('exiting...'));
+const app = express();
+const http = createServer(app);
+
+app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false }));
+app.get('/ice', (_, res) => {
+  res.header('content-type', 'application/json; charset=utf-8').send(servers);
+});
+app.use('/', expressStaticGzip('./public'));
+
+const senderWs = new Server(http, { path: '/signal/sender' });
+const receiverWs = new Server(http, { path: '/signal/receiver' });
+const controller = new JobController(senderWs, receiverWs);
+controller.init();
+
+http.listen(8080, () => {
+  console.log('[server] started listening on port 8080');
+});
+
+process.on('SIGTERM', () => {
+  debug('[server] SIGTERM signal received: closing HTTP server');
+  http.close();
+});

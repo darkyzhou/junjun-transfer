e58b6f823bd43fbe422d0d65f8b4686257a9089e
darkyzhou <darkyoooooo@gmail.com>
2022-04-18 21:57:17 +0800

    chore: rename folder

--- backend-app/index.js
+++
@@ -1,7 +0,0 @@
-const startupSignalServer = require('./lib/signal-server');
-const startupIceAdvertiser = require('./lib/ice-advertiser');
-const { addExitHook } = require('exit-hook-plus');
-
-startupSignalServer();
-startupIceAdvertiser();
-addExitHook(() => console.log('exiting...'));
